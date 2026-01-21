a1cb0c885cf9fff90606f975637a8b41da9f5c4b
25f2000129 <25f2000129@ds.study.iitm.ac.in>
2026-01-22 01:15:34 +0530

    Update generate_numbers.py

--- generate_numbers.py
+++ generate_numbers.py
@@ -1,13 +1,19 @@
 def natural_numbers(n):
     #Returns the list of natural numbers
+    ls = []
     while n>0:
-        print(n)
+        ls.append(n)
         n-=1
+    return n
 
 
 def nth_fibonacci(n):
-    #Returns n-th fibonacci number
-    #Code here
+    #Returns n-th fibonacci numbe
+    a, b = 0, 1
+    for i in range(n):
+        a, b = b, a + b
+    return a
+        
 
 def is_prime(n):
     #Return true if 'n' is prime, otherwise returns false
